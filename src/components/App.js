da28536a14b75011b763bb5587a930739aabdd79
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-16 12:56:06 -0600

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -112,7 +112,9 @@ export default function App() {
 
   useEffect(() => {
     // 🔥 STEP 9- ADJUST THE STATUS OF `disabled` EVERY TIME `formValues` CHANGES
-    
+    schema.isValid(formValues).then((valid) => {
+      setDisabled(!valid);
+    });
   }, []);
 
   return (
